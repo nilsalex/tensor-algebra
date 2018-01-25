9efe1728e0ac302fa53d905a42a77d58e32c777d
Nils Alex <nils@famalex.de>
2018-01-25 15:17:47 +0100

    rudimentary multicore support

--- src/NumericSimplify.cpp
+++ src/NumericSimplify.cpp
@@ -6,11 +6,16 @@
 #include <set>
 #include <vector>
 
+#include <future>
+#include <thread>
+
 #include <Eigen/Dense>
 
 #include <gmpxx.h>
 
 #include "Expression.h"
+
+int THREADS_NUM = std::thread::hardware_concurrency();
 
 typedef Eigen::Matrix<mpq_class, Eigen::Dynamic, Eigen::Dynamic> MatrixXq;
 
@@ -22,30 +27,59 @@ static std::ostream& operator<<(std::ostream & stream, mpq_class const & rop) {
 Expression Expression::NumericSimplify(Indices const & indices, bool print_matrix) const {
   assert(!this->IsZero());
 
-  std::set<ScalarSum> sum_set;
-  std::set<size_t> coefficient_set;
-
-  std::string n;
-
   std::bitset<64> bs(0);
   bs[2 * indices.size()] = true;
   size_t number_of_combinations = bs.to_ulong();
 
-  for (size_t counter = 0; counter < number_of_combinations; ++counter) {
-    std::vector<size_t> numbers (indices.size());
-    std::bitset<64> binary_representation(counter);
-    for(size_t digit_counter = 0; digit_counter < indices.size(); ++digit_counter) {
-      unsigned int binary_digit_a = static_cast<unsigned int>(binary_representation[2 * digit_counter + 1]);
-      unsigned int binary_digit_b = static_cast<unsigned int>(binary_representation[2 * digit_counter]);
-      *(numbers.rbegin() + digit_counter) = 2 * binary_digit_a + binary_digit_b;
-    }
+  auto thread_function = [expression=*this,noc=number_of_combinations,indices=indices] (size_t thread_counter, auto coefficient, auto sum) mutable -> void {
+    std::set<size_t> coefficient_set;
+    std::set<ScalarSum> sum_set;
+    for (size_t counter = thread_counter * noc / THREADS_NUM; counter < (thread_counter + 1) * noc / THREADS_NUM; ++counter) {
+      std::vector<size_t> numbers (indices.size());
+      std::bitset<64> binary_representation(counter);
+      for (size_t digit_counter = 0; digit_counter < indices.size(); ++digit_counter) {
+        unsigned int binary_digit_a = static_cast<unsigned int>(binary_representation[2 * digit_counter + 1]);
+        unsigned int binary_digit_b = static_cast<unsigned int>(binary_representation[2 * digit_counter]);
+        *(numbers.rbegin() + digit_counter) = 2 * binary_digit_a + binary_digit_b;
+      }
 
-    ScalarSum sum_tmp = EvaluateIndices(indices, numbers);
-    if (!sum_tmp.IsZero()) {
-      coefficient_set.merge(sum_tmp.CoefficientSet());
-      sum_set.insert(sum_tmp);
+      ScalarSum sum_tmp = expression.EvaluateIndices(indices, numbers);
+      if (!sum_tmp.IsZero()) {
+        coefficient_set.merge(sum_tmp.CoefficientSet());
+        sum_set.insert(sum_tmp);
+      }
     }
+    coefficient.set_value(coefficient_set);
+    sum.set_value(sum_set);
+  };
 
+  std::vector<std::thread> t (THREADS_NUM);
+
+  std::vector<std::promise<std::set<size_t>>> coefficients_promises (THREADS_NUM);
+  std::vector<std::promise<std::set<ScalarSum>>> sums_promises (THREADS_NUM);
+
+  std::vector<std::future<std::set<size_t>>> coefficients_futures (THREADS_NUM);
+  std::vector<std::future<std::set<ScalarSum>>> sums_futures (THREADS_NUM);
+
+  for (int thread_counter = 0; thread_counter < THREADS_NUM; ++thread_counter) {
+    coefficients_futures[thread_counter] = coefficients_promises[thread_counter].get_future();
+    sums_futures[thread_counter] = sums_promises[thread_counter].get_future();
+    std::cout << "Launching thread " << thread_counter << std::endl;
+    t[thread_counter] = std::thread(thread_function, thread_counter, std::move(coefficients_promises[thread_counter]),
+                                                                     std::move(sums_promises[thread_counter]));
+  }
+
+  for (int thread_counter = 0; thread_counter < THREADS_NUM; ++thread_counter) {
+    t[thread_counter].join();
+    std::cout << "Joined thread " << thread_counter << std::endl;
+  }
+
+  std::set<ScalarSum> sum_set;
+  std::set<size_t> coefficient_set;
+
+  for (int thread_counter = 0; thread_counter < THREADS_NUM; ++thread_counter) {
+    coefficient_set.merge(coefficients_futures.at(thread_counter).get());
+    sum_set.merge(sums_futures.at(thread_counter).get());
   }
 
   std::map<size_t, size_t> coefficient_map;
