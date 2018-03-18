5199f828e3060159c2888b672b29748797242caa
Nils Alex <nils@famalex.de>
2018-03-18 18:55:37 +0100

    project_cyclic() less memory usage

--- src/area_ansatz_cyclic.cpp
+++ src/area_ansatz_cyclic.cpp
@@ -31,124 +31,63 @@ int main () {
 
       auto project_cyclic = [] (Expression & expr) -> void {
           
-          Expression simplified_diff;
+          auto simplified_diff = std::make_unique<Expression>();
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          Indices free_indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'});
+          std::vector<std::pair<Indices, Rational>> symmetries_1 {
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'd', 'c', 'e', 'f', 'g', 'h', 'p', 'q'}), Rational(-1, 1)),
+              std::make_pair(Indices ({'a', 'c', 'b', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}), Rational(-1, 1)),
+              std::make_pair(Indices ({'a', 'c', 'd', 'b', 'e', 'f', 'g', 'h', 'p', 'q'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'd', 'b', 'c', 'e', 'f', 'g', 'h', 'p', 'q'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'd', 'c', 'b', 'e', 'f', 'g', 'h', 'p', 'q'}), Rational(-1, 1))
+            };
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'b', 'd', 'c', 'e', 'f', 'g', 'h', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          std::for_each(symmetries_1.cbegin(), symmetries_1.cend(),
+            [&simplified_diff,free_indices,&expr] (auto const & sym) {
+              Expression expr_tmp (expr);
+              expr_tmp.SubstituteFreeIndices(free_indices, sym.first);
+              expr_tmp.MultiplyCoefficient(sym.second);
+              simplified_diff->AddOther(expr_tmp);
+            });
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'c', 'b', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          simplified_diff->MultiplyCoefficient(Rational(-1, 6));
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'c', 'd', 'b', 'e', 'f', 'g', 'h', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          auto simplified_new = std::make_unique<Expression>(expr);
+          simplified_new->AddOther(*simplified_diff);
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'd', 'b', 'c', 'e', 'f', 'g', 'h', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          simplified_diff.reset(new Expression());
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'd', 'c', 'b', 'e', 'f', 'g', 'h', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          std::vector<std::pair<Indices, Rational>> symmetries_2 {
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'h', 'g', 'p', 'q'}), Rational(-1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'g', 'f', 'h', 'p', 'q'}), Rational(-1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'g', 'h', 'f', 'p', 'q'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'h', 'f', 'g', 'p', 'q'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'h', 'g', 'f', 'p', 'q'}), Rational(-1, 1)),
+            };
 
-          simplified_diff.MultiplyCoefficient(Rational(-1, 6));
+          std::for_each(symmetries_2.cbegin(), symmetries_2.cend(),
+            [&simplified_diff,free_indices,&simplified_new] (auto const & sym) {
+              Expression expr_tmp (*simplified_new);
+              expr_tmp.SubstituteFreeIndices(free_indices, sym.first);
+              expr_tmp.MultiplyCoefficient(sym.second);
+              simplified_diff->AddOther(expr_tmp);
+            });
 
-          Expression simplified_new_int(expr);
-          simplified_new_int.AddOther(simplified_diff);
+          simplified_diff->MultiplyCoefficient(Rational(-1, 6));
 
-          Expression simplified_diff_2;
+          simplified_new->AddOther(*simplified_diff);
+          simplified_diff.reset(nullptr);
 
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
+          simplified_new->ApplyMonomialSymmetries();
+          simplified_new->SortMonomials();
+          simplified_new->SortSummands();
+          simplified_new->CollectPrefactors();
+          simplified_new->CanonicalisePrefactors();
+          simplified_new->EliminateZeros();
 
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'h', 'g', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
-
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'g', 'f', 'h', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
-
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'g', 'h', 'f', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
-
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'h', 'f', 'g', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
-
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'p', 'q'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'h', 'g', 'f', 'p', 'q'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
-
-          simplified_diff_2.MultiplyCoefficient(Rational(-1, 6));
-
-          Expression expr_new(simplified_new_int);
-          expr_new.AddOther(simplified_diff_2);
-
-          expr_new.ApplyMonomialSymmetries();
-          expr_new.SortMonomials();
-          expr_new.SortSummands();
-          expr_new.CollectPrefactors();
-          expr_new.CanonicalisePrefactors();
-          expr_new.EliminateZeros();
-
-          expr = expr_new;
+          expr = *simplified_new;
         };
     
       Generator gtor (indices,  {std::make_pair(indices2, true),
@@ -189,126 +128,64 @@ int main () {
 
       auto project_cyclic = [] (Expression & expr) -> void {
           
-          Expression simplified_diff;
+          auto simplified_diff = std::make_unique<Expression>();
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          Indices free_indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'});
+          std::vector<std::pair<Indices, Rational>> symmetries_1 {
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'd', 'c', 'e', 'f', 'g', 'h'}), Rational(-1, 1)),
+              std::make_pair(Indices ({'a', 'c', 'b', 'd', 'e', 'f', 'g', 'h'}), Rational(-1, 1)),
+              std::make_pair(Indices ({'a', 'c', 'd', 'b', 'e', 'f', 'g', 'h'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'd', 'b', 'c', 'e', 'f', 'g', 'h'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'd', 'c', 'b', 'e', 'f', 'g', 'h'}), Rational(-1, 1))
+            };
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'b', 'd', 'c', 'e', 'f', 'g', 'h'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          std::for_each(symmetries_1.cbegin(), symmetries_1.cend(),
+            [&simplified_diff,free_indices,&expr] (auto const & sym) {
+              Expression expr_tmp (expr);
+              expr_tmp.SubstituteFreeIndices(free_indices, sym.first);
+              expr_tmp.MultiplyCoefficient(sym.second);
+              simplified_diff->AddOther(expr_tmp);
+            });
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'c', 'b', 'd', 'e', 'f', 'g', 'h'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          simplified_diff->MultiplyCoefficient(Rational(-1, 6));
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'c', 'd', 'b', 'e', 'f', 'g', 'h'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          auto simplified_new = std::make_unique<Expression>(expr);
+          simplified_new->AddOther(*simplified_diff);
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'd', 'b', 'c', 'e', 'f', 'g', 'h'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          simplified_diff.reset(new Expression());
 
-          {
-            Expression expr_tmp (expr);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'd', 'c', 'b', 'e', 'f', 'g', 'h'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff.AddOther(expr_tmp);
-          }
+          std::vector<std::pair<Indices, Rational>> symmetries_2 {
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'h', 'g'}), Rational(-1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'g', 'f', 'h'}), Rational(-1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'g', 'h', 'f'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'h', 'f', 'g'}), Rational(1, 1)),
+              std::make_pair(Indices ({'a', 'b', 'c', 'd', 'e', 'h', 'g', 'f'}), Rational(-1, 1)),
+            };
 
-          simplified_diff.MultiplyCoefficient(Rational(-1, 6));
+          std::for_each(symmetries_2.cbegin(), symmetries_2.cend(),
+            [&simplified_diff,free_indices,&simplified_new] (auto const & sym) {
+              Expression expr_tmp (*simplified_new);
+              expr_tmp.SubstituteFreeIndices(free_indices, sym.first);
+              expr_tmp.MultiplyCoefficient(sym.second);
+              simplified_diff->AddOther(expr_tmp);
+            });
 
-          Expression simplified_new_int(expr);
-          simplified_new_int.AddOther(simplified_diff);
+          simplified_diff->MultiplyCoefficient(Rational(-1, 6));
 
-          Expression simplified_diff_2;
+          simplified_new->AddOther(*simplified_diff);
+          simplified_diff.reset(nullptr);
 
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
+          simplified_new->ApplyMonomialSymmetries();
+          simplified_new->SortMonomials();
+          simplified_new->SortSummands();
+          simplified_new->CollectPrefactors();
+          simplified_new->CanonicalisePrefactors();
+          simplified_new->EliminateZeros();
 
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'h', 'g'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
-
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'g', 'f', 'h'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
-
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'g', 'h', 'f'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
-
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'h', 'f', 'g'}));
-            expr_tmp.MultiplyCoefficient(Rational(1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
-
-          {
-            Expression expr_tmp (simplified_new_int);
-            expr_tmp.SubstituteFreeIndices(Indices ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}),
-                                           Indices ({'a', 'b', 'c', 'd', 'e', 'h', 'g', 'f'}));
-            expr_tmp.MultiplyCoefficient(Rational(-1, 1));
-            simplified_diff_2.AddOther(expr_tmp);
-          }
-
-          simplified_diff_2.MultiplyCoefficient(Rational(-1, 6));
-
-          Expression expr_new(simplified_new_int);
-          expr_new.AddOther(simplified_diff_2);
-
-          expr_new.ApplyMonomialSymmetries();
-          expr_new.SortMonomials();
-          expr_new.SortSummands();
-          expr_new.CollectPrefactors();
-          expr_new.CanonicalisePrefactors();
-          expr_new.EliminateZeros();
-
-          expr = expr_new;
+          expr = *simplified_new;
         };
-    
       Generator gtor (indices,  {std::make_pair(indices2, true),
                                  std::make_pair(indices3, true),
                                  std::make_pair(indices4, true),
